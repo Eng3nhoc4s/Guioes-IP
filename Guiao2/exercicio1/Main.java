eaeb4cf6f2ed3c5ef5a67d6f903a295a5cfff167
Rodrigo Reis <N-genhocas@hotmail.com>
2017-02-12 20:18:27 +0000

    Exercicio 1

---
+++ Guiao2/exercicio1/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+		//Alinea 1
+		String diaDaSemana = "Quarta-feira";
+		int dia = 15;
+		String mes = "Fevereiro";
+		int ano = 2017;
+		
+		//Alinea 2
+		System.out.println(diaDaSemana);
+		System.out.println(dia);
+		System.out.println(mes);
+		System.out.println(ano);
+		
+		//Alinea 3
+		System.out.println(diaDaSemana + ", " +  dia + " de " + mes + " de " + ano);
+		
+		//Alinea 4
+		System.out.println(diaDaSemana + ", " +  mes + " " + dia + ", " + ano);
+	}
+}
