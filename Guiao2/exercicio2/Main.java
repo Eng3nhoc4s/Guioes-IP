dec32e5be2620a0e3791bcc7876d5bcbe7fdc9be
Rodrigo Reis <N-genhocas@hotmail.com>
2017-02-12 20:21:07 +0000

    Resolução do exercício 2

---
+++ Guiao2/exercicio2/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+		int i = 1, 
+			j = 2, 
+			k = 4;
+		
+		float media = ((i + j + k) / 3.0F);
+		
+		System.out.println("Media: " +  media);
+	}
+}
